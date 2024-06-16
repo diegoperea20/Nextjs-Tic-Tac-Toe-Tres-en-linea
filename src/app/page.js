364fa6d45a5050571628c4044e6a22202300a921
diegoperea20 <diegoperea20@gmail.com>
2024-06-16 15:29:18 -0500

    added link

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ const Home = () => {
       <Game />
       <div className="project-github">
       <p>This project is in </p>
-      <Link href="https://github.com/diegoperea20">
+      <Link href="https://github.com/diegoperea20/Nextjs-Tic-Tac-Toe-Tres-en-linea">
         <img width="96" height="96" src="https://img.icons8.com/fluency/96/github.png" alt="github"/>
       </Link>
     </div>
